0c307679f9b3d8d0b4c5d6e025ff8164eb8269ec
Rasintha_Rukshan <Rasintharukshanp@gmail.com>
2026-05-17 11:43:17 +0530

    INIT: Spring Boot Project

---
+++ src/main/java/com/rukshan/ranaswanu/RanaswanuApplication.java
@@ -0,0 +1,13 @@
+package com.rukshan.ranaswanu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanaswanuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RanaswanuApplication.class, args);
+    }
+
+}
